d5b5ad01baeb79bc388f6a9d8c0abab9e28e0550
Nando San Miguel <mythemc@gmail.com>
2023-03-22 17:32:34 -0600

    feat: :lipstick: add footer and styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { Route } from 'wouter'
+import Footer from './components/Footer'
 import Home from './pages/Home'
 
 function App () {
   return (
     <div className='App'>
       <Route path='' component={Home} />
+      <Footer />
     </div>
   )
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,18 @@
+import styles from './Footer.module.css'
+
+function Footer () {
+  return (
+    <footer className={styles.main}>
+      <section>2020 AtomicLabs. Todos los derechos reservados.</section>
+      <section>
+        <a href='#'>Aviso de privacidad</a>
+        <div>
+          Tw
+          Lk
+        </div>
+      </section>
+    </footer>
+  )
+}
+
+export default Footer
