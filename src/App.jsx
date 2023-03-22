d2426f00b68b5fed9c4cc811d6b8259684462883
Nando San Miguel <mythemc@gmail.com>
2023-03-22 17:19:12 -0600

    feat: :sparkles: move components and add wouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
-import './App.css'
+import { Route } from 'wouter'
+import Home from './pages/Home'
 
 function App () {
   return (
-    <div className='App' />
+    <div className='App'>
+      <Route path='' component={Home} />
+    </div>
   )
 }
 
---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import './Home.css'
+
+function Home () {
+  return (
+    <main className='home'>
+      Hey
+    </main>
+  )
+}
+
+export default Home
