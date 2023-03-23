e262ce9d1ea3fab0729161d74a33502d22daab74
Nando San Miguel <mythemc@gmail.com>
2023-03-22 19:03:51 -0600

    feat: :lipstick: improve styles and features in button

---
+++ src/components/Button.jsx
@@ -0,0 +1,21 @@
+import styles from './Button.module.css'
+
+function Button ({ title, onClick, type, disabled }) {
+  const typeStyle = type === 'secondary' ? styles.secondary : styles.primary
+  const buttonClassName = [styles.button, typeStyle].join(' ')
+
+  return (
+    <div>
+      <button
+        className={buttonClassName}
+        type='button'
+        onClick={onClick}
+        disabled={disabled}
+      >
+        {title}
+      </button>
+    </div>
+  )
+}
+
+export default Button
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,20 @@
 import './Home.css'
 import astronaut from '../assets/astronaut.png'
+import Button from '../components/Button'
 
 function Home () {
   return (
     <main className='home'>
       <section className='headline'>
         <article>
-          <img src={astronaut} alt='Astronaut wearing the suit labeled with Atomic32' />
+          <img
+            src={astronaut}
+            alt='Astronaut wearing the suit labeled with Atomic32'
+          />
         </article>
         <article>
           <p>Desarrolla todo tu POTENCIAL dentro del equipo ATOMICLABS</p>
-          <a href='#'>¡Quiero ser parte!</a>
+          <Button title='¡Quiero ser parte!' />
         </article>
       </section>
     </main>
