a8c098b6b5ca78f1c4535d4dd05901d017978548
Nando San Miguel <mythemc@gmail.com>
2023-03-23 12:46:31 -0600

    style: :lipstick: update images

--- src/components/FooterHome.jsx
+++ src/components/FooterHome.jsx
@@ -8,21 +8,27 @@ function FooterHome ({ className }) {
       <main>
         <section>
           <article>
-            <img src={astronautDrawing} alt='Astronaut with a digital tablet and a mouse' />
+            <div>
+              <img src={astronautDrawing} alt='Astronaut with a digital tablet and a mouse' />
+            </div>
             <ul>
               <li>Usamos las tecnologías más modernas.</li>
               <li>Innovamos y creamos proyectos retadores.</li>
             </ul>
           </article>
           <article>
-            <img src={astronautComputer} alt='Astronaut typing with a hand in a laptop' />
+            <div>
+              <img src={astronautComputer} alt='Astronaut typing with a hand in a laptop' />
+            </div>
             <ul>
               <li>¡Trabajamos en equipo rumbo al éxito!</li>
               <li>No tenemos código de vestimenta</li>
             </ul>
           </article>
           <article>
-            <img src={astronautPose} alt='Astronaut posing with a hand' />
+            <div>
+              <img src={astronautPose} alt='Astronaut posing with a hand' />
+            </div>
             <ul>
               <li>Realizamos actividades para tu bienestar.</li>
               <li>¡Tenemos un parque frente a la oficina!</li>
