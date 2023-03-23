e7c9f7cc6fa8f1bffa78a00add0a1ab307817a24
Nando San Miguel <mythemc@gmail.com>
2023-03-22 18:29:34 -0600

    feat: :sparkles: add the new headline

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,18 @@
 import './Home.css'
+import astronaut from '../assets/astronaut.png'
 
 function Home () {
   return (
     <main className='home'>
-      Hey
+      <section className='headline'>
+        <article>
+          <img src={astronaut} alt='Astronaut wearing the suit labeled with Atomic32' />
+        </article>
+        <article>
+          <p>Desarrolla todo tu POTENCIAL dentro del equipo ATOMICLABS</p>
+          <a href='#'>¡Quiero ser parte!</a>
+        </article>
+      </section>
     </main>
   )
 }
