2a38ab8e0e65626a2df2f5160c2694acd5912ab8
Nando San Miguel <mythemc@gmail.com>
2023-03-25 11:59:39 -0600

    refactor: :recycle: refactor the static info from the forms and prepare form

--- src/components/HeaderForm.jsx
+++ src/components/HeaderForm.jsx
@@ -1,22 +1,19 @@
 import styles from 'styles/HeaderForm.module.css'
-import HEADERS_DATA from 'static/headers.json'
 
-function HeaderForm ({ step }) {
-  const { title, titleAlt, src } = HEADERS_DATA[step]
+function HeaderForm ({ title, src, step }) {
+  const { main, alt } = title
   const URI_IMAGE = `/src/assets/${src}`
+
   return (
     <main className={styles.main}>
       {src
         ? (
-          <img
-            src={URI_IMAGE}
-            alt={`Orange circle with the number ${step + 1}`}
-          />
+          <img src={URI_IMAGE} alt={`Orange circle with the number ${step + 1}`} />
           )
         : null}
       <h1>
-        {title}
-        <span>{titleAlt}</span>
+        {main}
+        <span>{alt}</span>
       </h1>
     </main>
   )
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,10 +1,15 @@
 import HeaderForm from 'src/components/HeaderForm'
 import styles from 'styles/Contact.module.css'
+import HEADERS_DATA from 'static/headers.json'
+import { useState } from 'react'
 
 function Contact () {
+  const [step, setStep] = useState(0)
+  const { title, body, src } = HEADERS_DATA[step]
+
   return (
     <main className={styles.main}>
-      <HeaderForm step={0} />
+      <HeaderForm step={step} title={title} src={src} />
     </main>
   )
 }
