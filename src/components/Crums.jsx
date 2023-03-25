cbc39d84669e9cbd04e776f788f445d8042356c3
Nando San Miguel <mythemc@gmail.com>
2023-03-24 18:30:14 -0600

    feat: :sparkles: finish landing page

--- src/components/Crums.jsx
+++ src/components/Crums.jsx
@@ -1,11 +1,23 @@
 import styles from 'styles/Crums.module.css'
-import rightArrow from 'assets/rightArrow.svg'
 
 function Icon () {
   return (
-    <div className={styles.icon}>
-      <img src={rightArrow} alt='Arrow pointin to right' />
-    </div>
+    <svg
+      width='3em'
+      height='3em'
+      viewBox='0 0 24 24'
+      xmlns='http://www.w3.org/2000/svg'
+      className={styles.icon}
+    >
+      <path
+        d='M3 12h18m-3 3 3-3-3-3'
+        style={{
+          strokeLinecap: 'round',
+          strokeLinejoin: 'round',
+          strokeWidth: 1.5
+        }}
+      />
+    </svg>
   )
 }
 
@@ -15,7 +27,9 @@ function Crums () {
       <p>Contratación remota</p>
       <Icon />
       <p>Entrevista con el área de RH</p>
+      <Icon />
       <p>Prueba práctica</p>
+      <Icon />
       <p>Entrevista técnica</p>
     </section>
   )
