b26a5719afa9c938eb1d1b6a8c6bf14e7cb06605
Nando San Miguel <mythemc@gmail.com>
2023-03-26 00:08:07 -0600

    feat: :sparkles: add new body steps to handle the forms using conditional render

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,24 +1,30 @@
 import styles from 'styles/Button.module.css'
 import { Link } from 'wouter'
 
-function Button ({ title, onClick, type, disabled, href }) {
+function Button ({ title, onClick, type, disabled, href, className }) {
   const typeStyle = type === 'secondary' ? styles.secondary : styles.primary
   const buttonClassName = [styles.button, typeStyle].join(' ')
   const Wrapper = ({ children }) =>
-    href ? <Link href={href}>{children}</Link> : <div>{children}</div>
+    href
+      ? (
+        <Link href={href} className={className}>
+          {children}
+        </Link>
+        )
+      : (
+        <div className={className}>{children}</div>
+        )
 
   return (
     <Wrapper>
-      <div>
-        <button
-          className={buttonClassName}
-          type='button'
-          onClick={onClick}
-          disabled={disabled}
-        >
-          {title}
-        </button>
-      </div>
+      <button
+        className={buttonClassName}
+        type='button'
+        onClick={onClick}
+        disabled={disabled}
+      >
+        {title}
+      </button>
     </Wrapper>
   )
 }
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,15 +1,71 @@
+import { useState } from 'react'
 import HeaderForm from 'src/components/HeaderForm'
 import styles from 'styles/Contact.module.css'
 import HEADERS_DATA from 'static/headers.json'
-import { useState } from 'react'
+import Button from 'src/components/Button'
+
+function getBody (body, data, step) {
+  const { main } = body
+  const alt = body?.alt
+  const phone = data?.phone
+
+  const stepBody = {
+    0: (
+      <>
+        <p>{main}</p>
+      </>
+    ),
+    1: (
+      <>
+        <p>{main}</p>
+        <span>{alt}</span>
+      </>
+    ),
+    2: (
+      <>
+        <p>{main}</p>
+        <p>{phone}</p>
+        <span>{alt}</span>
+      </>
+    ),
+    3: (
+      <>
+        <p>{main}</p>
+        <span>{alt}</span>
+      </>
+    )
+  }
+  return stepBody[step]
+}
 
 function Contact () {
   const [step, setStep] = useState(0)
-  const { title, body, src } = HEADERS_DATA[step]
+  const { title, body, src, button } = HEADERS_DATA[step]
+  const handleClick = () => {
+    setStep(step + 1)
+  }
+  const handleStepDown = () => setStep(step - 1)
 
   return (
     <main className={styles.main}>
-      <HeaderForm step={step} title={title} src={src} />
+      <section>
+        {step ? <a onClick={handleStepDown}>{'< Regresar'}</a> : null}
+        <HeaderForm step={step} title={title} src={src} />
+        <article>{getBody(body, null, step)}</article>
+        {button
+          ? (
+            <Button
+              title={button}
+              onClick={handleClick}
+              type='secondary'
+              className={styles.button}
+            />
+            )
+          : null}
+      </section>
+      <section>
+        <img src='a' alt='a' />
+      </section>
     </main>
   )
 }
