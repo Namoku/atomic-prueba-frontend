142481aa8396ba01f96a1824cd65db5b71bc1bc8
Nando San Miguel <mythemc@gmail.com>
2023-03-26 15:07:54 -0600

    feat: :ambulance: move imports to the contact to load all images at once

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -8,7 +8,25 @@ import Modal from 'src/components/Modal'
 import Stepper from 'src/components/Stepper'
 import styles from 'styles/Contact.module.css'
 import HEADERS_DATA from 'static/headers.json'
-import getImage from './utils'
+import form1 from 'assets/form1.png'
+import form2 from 'assets/form2.png'
+import form3 from 'assets/form3.png'
+import form4 from 'assets/form4.png'
+import step1 from 'assets/step1.png'
+import step2 from 'assets/step2.png'
+import step3 from 'assets/step3.png'
+import step4 from 'assets/step4.png'
+
+const getImage = (id) => {
+  const IMAGES = {
+    0: { stepImg: step1, formImg: form1 },
+    1: { stepImg: step2, formImg: form2 },
+    2: { stepImg: step3, formImg: form2 },
+    3: { stepImg: step4, formImg: form3 },
+    4: { stepImg: null, formImg: form4 }
+  }
+  return IMAGES[id]
+}
 
 function getBody (body, data, setData, step, setOpenModal) {
   const { main } = body
